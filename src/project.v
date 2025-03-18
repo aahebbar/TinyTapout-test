7db82735abc16493f68a8c3e99f524df38e76e31
AvyakthaHebbar <132642742+aahebbar@users.noreply.github.com>
2025-03-18 10:48:44 +0530

    Updated clockDivider into project.v

--- src/project.v
+++ src/project.v
@@ -17,11 +17,45 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    assign uo_out[6:1]  = 6'b0;  // Example: ou_out is the sum of ui_in and uio_in
+    assign uo_out[0] = clockDivider;
+    assign uo_out[7] = &{ena, clk, rst_n, 1'b0} || &ui_in; 
+    assign uio_out = 0;
+    assign uio_oe  = 1;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  //wire _unused = &{ena, clk, rst_n, 1'b0};
 
+  wire reset, clockDivider;
+  assign reset = ~rst_n;
+  assign 
+    
+    clockDivider clockDiv (
+        .clk(clk),
+        .reset(reset),
+        .clockDivider(clockDivider)
+    );
+  
+endmodule
+
+
+module clockDivider (
+    input clk,
+    input reset,
+    output reg dividedClock
+);
+    reg [9:0] count;
+
+    always @ (posedge clk, posedge reset) begin
+        if (reset) begin
+            count <= 10'd24;
+            dividedClock <= 1'b0;
+        end else if (count == 1023) begin
+            count <= 10'd24;
+            dividedClock <= ~dividedClock;
+        end else begin
+            count <= count + 1'b1;
+            dividedClock <= dividedClock;
+        end
+    end
 endmodule
