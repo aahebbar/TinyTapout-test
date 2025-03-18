ec9521f452cef519a30ccdf716402e6f5bb719c5
AvyakthaHebbar <132642742+aahebbar@users.noreply.github.com>
2025-03-18 11:26:44 +0530

    Updated project.v

--- src/project.v
+++ src/project.v
@@ -19,7 +19,7 @@ module tt_um_example (
   // All output pins must be assigned. If not used, assign to 0.
     assign uo_out[6:1]  = 6'b0;  // Example: ou_out is the sum of ui_in and uio_in
     assign uo_out[0] = clockDivider;
-    assign uo_out[7] = &{ena, clk, rst_n, 1'b0} || &ui_in; 
+    assign uo_out[7] = &{ena, clk, rst_n, 1'b0} && &ui_in; 
     assign uio_out = 0;
     assign uio_oe  = 1;
 
